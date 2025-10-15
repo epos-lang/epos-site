006f29578b660d9cf51f827c94b5310432ab3b10
silent-brad <bradscottwhite@gmail.com>
2025-10-15 18:20:50 -0400

    change flake.nix to deploy

--- flake.nix
+++ flake.nix
@@ -10,29 +10,12 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        packages.default = pkgs.writeShellScriptBin "serve" ''
-          function run {
-            cp src/index.html docs/index.html
-            rm -rf docs/assets/*
-            cp -r src/assets/* docs/assets/
-            ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m &
-          }
-          run
-          function run_on_change {
-            ${pkgs.inotify-tools}/bin/inotifywait -e modify -e move -e create -e delete -r src/. | while read -r directory events file; do
-              run
-            done
-          }
-          run_on_change &
-
-          #${pkgs.static-web-server}/bin/static-web-server -p 8084 -d docs/.
+        packages.default = pkgs.writeShellScriptBin "build" ''
+          rm -rf docs
+          mkdir docs docs/assets
+          cp src/index.html docs/index.html
+          cp -r public/assets/* docs/assets
+          ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m
         '';
-
-        devShells.default = pkgs.mkShell {
-          #packages = with pkgs; [ static-web-server ];
-          shellHook = ''
-            echo "Run './result/bin/serve'"
-          '';
-        };
       });
 }
