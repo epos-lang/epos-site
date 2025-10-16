6280d97a4cb761397b13958f35acd85f80a55322
silent-brad <bradscottwhite@gmail.com>
2025-10-16 11:20:03 -0400

    learning docs

--- flake.nix
+++ flake.nix
@@ -17,12 +17,12 @@
           echo "epos-lang.org" > docs/CNAME
           cp src/index.html docs/index.html
           cp -r public/assets/* docs/assets
+          ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m
 
           cp src/learn/*.pdf docs/learn/
+          ${pkgs.tailwindcss}/bin/tailwindcss -i src/learn/style.css -o docs/learn/style.css -m
           ${learn.packages.${system}.default}/bin/build-docs
           cp src/learn/html/*.html docs/learn/
-
-          ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m
         '';
       });
 }
--- src/learn/main.py
+++ src/learn/main.py
@@ -3,6 +3,13 @@ import jinja2
 import os
 
 
+def render_toc(pages):
+    template = jinja2.Template(open('learn.jinja').read())
+    full_content = template.render(pages=pages)
+    with open("html/learn.html", 'w') as f:
+        f.write(full_content)
+
+
 def template(title, pdf, content):
     template = jinja2.Template(open('template.jinja').read())
     return template.render(title=title, pdf=pdf, content=content)
@@ -14,13 +21,16 @@ def render_file(path):
     full_content = template(title, path.replace("typst", "pdf"), content)
     with open("html/" + path.replace("typst", "html"), 'w') as f:
         f.write(full_content)
+    return {"title": title, "url": path.replace(".html", "")}
 
 
 # Delete all html files in the html folder
-for path in os.listdir('src/learn/html'):
-    if path.endswith('.html'):
-        os.remove('html/' + path)
+# for path in os.listdir('src/learn/html'):
+#    if path.endswith('.html'):
+#        os.remove('html/' + path)
 
 typst_files = [path for path in os.listdir('.') if path.endswith('.typst')]
+pages = []
 for path in typst_files:
-    render_file(path)
+    pages.append(render_file(path))
+render_toc(pages)
