2e4828dcaa4dbd5e691d1cc7ef9b510979f3f9d6
silent-brad <bradscottwhite@gmail.com>
2025-10-16 10:30:30 -0400

    added learning docs

--- flake.nix
+++ flake.nix
@@ -4,18 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    learn.url = "path:./src/learn";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, learn }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
         packages.default = pkgs.writeShellScriptBin "build" ''
           rm -rf docs
-          mkdir docs docs/assets
+          mkdir docs docs/assets docs/learn
           echo "epos-lang.org" > docs/CNAME
           cp src/index.html docs/index.html
           cp -r public/assets/* docs/assets
+
+          cp src/learn/*.pdf docs/learn/
+          ${learn.packages.${system}.default}/bin/build-docs
+          cp src/learn/*.html docs/learn/
+
           ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m
         '';
       });
--- src/learn/main.py
+++ src/learn/main.py
@@ -1,18 +1,22 @@
 import pypandoc
 import jinja2
+import os
 
 
-def template(title, content):
+def template(title, pdf, content):
     template = jinja2.Template(open('template.jinja').read())
-    return template.render(title=title, content=content)
+    return template.render(title=title, pdf=pdf, content=content)
 
 
 def render_file(path):
     content = pypandoc.convert_file(path, 'html5')
     title = path.split('.')[0].replace('-', ' ').capitalize()
-    return template(title, content)
+    full_content = template(title, path.replace("typst", "pdf"), content)
+    with open(path.replace("typst", "html"), 'w') as f:
+        f.write(full_content)
 
 
-full_content = render_file('index.typst')
-with open('index.html', 'w') as f:
-    f.write(full_content)
+# Runs render_file on all "typst" files in the current directory
+typst_files = [path for path in os.listdir('.') if path.endswith('.typst')]
+for path in typst_files:
+    render_file(path)
