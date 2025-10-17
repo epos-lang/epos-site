abccd079f9313638c367e8ce933250e76c5c18f3
silent-brad <bradscottwhite@gmail.com>
2025-10-17 17:50:21 -0400

    added new styling

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    learn.url = "path:./src/learn";
+    learn.url = "path:./src";
   };
 
   outputs = { self, nixpkgs, flake-utils, learn }:
@@ -17,16 +17,18 @@
           echo "epos-lang.org" > docs/CNAME
           cp src/index.html docs/index.html
           cp -r assets/* docs/assets
+          cp src/code.css docs/code.css
+          cp src/code.js docs/code.js
           ${pkgs.tailwindcss}/bin/tailwindcss -i src/style.css -o docs/style.css -m
 
-          find src/learn -type f -name "*.pdf" | while read -r file; do
+          find src -type f -name "*.pdf" | while read -r file; do
             filename=$(basename "$file")
             new_filename=$(echo "$filename" | sed 's/^[0-9]\+-\(.*\)/\1/')
-            cp "$file" "docs/learn/$new_filename"
+            cp "$file" "docs/$new_filename"
           done
-          ${pkgs.tailwindcss}/bin/tailwindcss -i src/learn/style.css -o docs/learn/style.css -m
+          ${pkgs.tailwindcss}/bin/tailwindcss -i src/learn.css -o docs/learn.css -m
           #${learn.packages.${system}.default}/bin/build-docs
-          cp src/learn/html/*.html docs/learn/
+          cp src/html/*.html docs/
         '';
       });
 }
